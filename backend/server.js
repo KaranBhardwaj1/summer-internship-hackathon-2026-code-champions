fa8a5bf7b7b872260fa8000f82e6fac184aaacef
Karan Bhardwaj <karanbhardwaj0077@gmail.com>
2026-07-03 10:47:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-const connectDB = require("./config/db");
-
 dotenv.config();
 
-connectDB();
+const connectDB = require("./config/db");
 
 const app = express();
 
+connectDB();
+
 app.use(cors());
 app.use(express.json());
+
 const aiRoutes = require("./routes/aiRoutes");
 app.use("/api/ai", aiRoutes);
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
+
 const carbonRoutes = require("./routes/carbonRoutes");
 app.use("/api/carbon", carbonRoutes);
 
@@ -25,8 +26,4 @@ app.get("/", (req, res) => {
   res.send("CarbonIQ API Running");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
