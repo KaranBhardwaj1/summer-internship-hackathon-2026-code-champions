3de8eb160a95c9b8c23d9d35a99261729edfcba3
Karan Bhardwaj <karanbhardwaj0077@gmail.com>
2026-08-06 02:59:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,4 +26,8 @@ app.get("/", (req, res) => {
   res.send("CarbonIQ API Running");
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
