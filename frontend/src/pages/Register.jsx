03229d79dc08dfed9041ddf0142b5ef7322cea1c
Karan Bhardwaj <karanbhardwaj0077@gmail.com>
2026-08-06 03:09:09 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { registerUser } from "../services/auth";
 import { Link } from "react-router-dom";
+import "../styles/Login.css";
 
 function Register() {
   const navigate = useNavigate();
